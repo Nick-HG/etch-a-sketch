d40a50a49594b3f0bfe5b11b18e9f5985bba9de4
Nick Herring <nkherring94@gmail.com>
2022-08-19 10:09:15 +0100

    Refactor JS and applied styles - grid visible

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-// create a 16x16 grid
-const container = document.querySelector('.container');
+const container = document.getElementById('container');
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
 
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
 
-for(i = 0; i < 16; i++) {
-    const divSquare = document.createElement('div');
-    divSquare.className = 'square';
-    document.querySelector('.container').appendChild(divSquare);
-};
+function makeRows(rowNum) {
+    for(r = 0; r < rowNum; r++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = 'gridRow' ;
+    };
+}
+
+function makeColumns(cellNum) {
+    for(c = 0; c < rows.length; c++) {
+        for(i = 0; i < cellNum; i++) {
+            let newCell = document.createElement('div');
+            rows[i].appendChild(newCell).className = 'cell';
+        };
+    };
+}
+
+defaultGrid();
