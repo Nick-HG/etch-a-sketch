e00f82b71509932bc9ba60673c906fee492b08fd
Nick Herring <nkherring94@gmail.com>
2022-08-19 16:39:04 +0100

    Center content, change grid size & apply colors

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-const container = document.getElementById('container');
-let rows = document.getElementsByClassName('gridRow');
-let cells = document.getElementsByClassName('cell');
+let color = 'black';
 
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
-}
-
-function makeRows(rowNum) {
-    for(r = 0; r < rowNum; r++) {
-        let row = document.createElement('div');
-        container.appendChild(row).className = 'gridRow' ;
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size;
+    for(let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
     };
 }
 
-function makeColumns(cellNum) {
-    for(c = 0; c < rows.length; c++) {
-        for(i = 0; i < cellNum; i++) {
-            let newCell = document.createElement('div');
-            rows[i].appendChild(newCell).className = 'cell';
-        };
-    };
+populateBoard(32);
+
+
+function changeSize(input) {
+    if(input < 100) {
+        populateBoard(input);
+    } else {
+        console.error('too many square');
+    }
 }
 
-defaultGrid();
+function colorSquare() {
+    if(color === 'random') {
+        this.style.backgroundColor = "#" + (Math.random() * 0xFFFFFF<<0).toString(16);
+    } else {
+        this.style.backgroundColor = color;
+    }
+    
+}
+
+function changeColor(choice) {
+    color = choice;
+}
